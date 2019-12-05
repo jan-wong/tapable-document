667b20654123959e090c9ad872f2cdf2e1440692
jan-wong <543050768@qq.com>
2019-12-05 22:18:07 +0800

    synchook

---
+++ sync.js
@@ -0,0 +1,47 @@
+const { SyncHook } = require('tapable')
+
+class Work {
+  constructor() {
+    this.hooks = {
+      plan: new SyncHook(),
+      working: new SyncHook(),
+      complete: new SyncHook()
+    }
+  }
+
+  tap() {
+    this.hooks.plan.tap('planTs', () => {
+      console.log('plan typescript')
+    })
+
+    this.hooks.plan.tap('planDesignPattern', () => {
+      console.log('plan design pattern')
+    })
+
+    this.hooks.working.tap('workTs', () => {
+      console.log('work typescript')
+    })
+
+    this.hooks.working.tap('workDesignPattern', () => {
+      console.log('work desing pattern')
+    })
+
+    this.hooks.complete.tap('completeTs', () => {
+      console.log('complete typescript')
+    })
+
+    this.hooks.complete.tap('completeDesignPattern', () => {
+      console.log('complete design pattern')
+    })
+
+    return this
+  }
+
+  run() {
+    this.hooks.plan.call()
+    this.hooks.working.call()
+    this.hooks.complete.call()
+  }
+}
+
+(new Work()).tap().run()
